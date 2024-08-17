ad340eaf9db47586ea81c106442adeaa101bcacb
Kelson Ameida <kelson.victr@gmail.com>
2024-08-17 16:06:42 -0300

    first commit

---
+++ src/main/java/br/com/gerenciadordeprodutos/api/GerenciadorDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadordeprodutos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gerenciadordeprodutos/api/GerenciadorDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadordeprodutos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
