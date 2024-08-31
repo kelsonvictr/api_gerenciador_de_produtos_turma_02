ac50cf43f80b8728b760917db47f58e15feeab53
Kelson Ameida <kelson.victr@gmail.com>
2024-08-31 10:54:17 -0300

    feat: Aula 10 inicio

---
+++ src/main/java/br/com/gerenciadordeprodutos/api/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package br.com.gerenciadordeprodutos.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    /*
+       Vai ficar disponível a doc swagger na url: http://localhost:8080/swagger-ui/index.html
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
