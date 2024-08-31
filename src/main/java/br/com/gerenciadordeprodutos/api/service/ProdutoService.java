b3755618471e06cd3da3cd76bb066f3f01b0e00b
Kelson Ameida <kelson.victr@gmail.com>
2024-08-31 11:28:54 -0300

    feat: Aula 10 contrato produto

--- src/main/java/br/com/gerenciadordeprodutos/api/service/ProdutoService.java
+++ src/main/java/br/com/gerenciadordeprodutos/api/service/ProdutoService.java
@@ -1,4 +1,19 @@
 package br.com.gerenciadordeprodutos.api.service;
 
+import br.com.gerenciadordeprodutos.api.dtos.CriarProdutoRequest;
+import br.com.gerenciadordeprodutos.api.dtos.ProdutoCriadoResponse;
+import br.com.gerenciadordeprodutos.api.model.Produto;
+
+import java.util.List;
+
 public interface ProdutoService {
+    ProdutoCriadoResponse criarProduto(CriarProdutoRequest criarProdutoRequest);
+
+    Produto buscarProdutoPeloId(Long id);
+
+    List<Produto> buscarTodosProdutos();
+
+    Produto atualizarProduto(Long id, CriarProdutoRequest criarProdutoRequest);
+
+    void deletarProdutoPeloId(Long id);
 }
