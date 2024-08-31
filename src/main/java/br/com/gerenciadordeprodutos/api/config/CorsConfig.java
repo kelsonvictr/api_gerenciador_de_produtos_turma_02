77ef5608dc92e2965a7043e72e0109b4ecceb3bc
Kelson Ameida <kelson.victr@gmail.com>
2024-08-31 15:08:24 -0300

    feat: Aula 10 CORS Config

---
+++ src/main/java/br/com/gerenciadordeprodutos/api/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package br.com.gerenciadordeprodutos.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173"); // Adicione o endereço do seu aplicativo React
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
