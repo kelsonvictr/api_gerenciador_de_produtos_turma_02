ad5219a001514bcdbb0919c6e540494f830528ad
kelsonalmeida <kelson.victr@gmail.com>
2024-09-28 13:05:05 -0300

    Correção atualizar endereco fornecedor

--- src/main/java/br/com/gerenciadordeprodutos/api/service/FornecedorServiceImpl.java
+++ src/main/java/br/com/gerenciadordeprodutos/api/service/FornecedorServiceImpl.java
@@ -67,10 +67,25 @@ public class FornecedorServiceImpl implements FornecedorService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Fornecedor não encontrado"));
 
+        Endereco endereco = fornecedorExistente.getEndereco();
+        if (endereco == null) {
+            endereco = new Endereco();
+        }
+
+        endereco.setLogradouro(criarFornecedorRequest.endereco().logradouro());
+        endereco.setNumero(criarFornecedorRequest.endereco().numero());
+        endereco.setComplemento(criarFornecedorRequest.endereco().complemento());
+        endereco.setBairro(criarFornecedorRequest.endereco().bairro());
+        endereco.setCidade(criarFornecedorRequest.endereco().cidade());
+        endereco.setEstado(criarFornecedorRequest.endereco().estado());
+        endereco.setPais(criarFornecedorRequest.endereco().pais());
+        endereco.setCep(criarFornecedorRequest.endereco().cep());
+
         fornecedorExistente.setNome(criarFornecedorRequest.nome());
         fornecedorExistente.setCnpj(criarFornecedorRequest.cnpj());
         fornecedorExistente.setEmail(criarFornecedorRequest.email());
         fornecedorExistente.setTipoFornecedor(criarFornecedorRequest.tipoFornecedor());
+        fornecedorExistente.setEndereco(endereco);
 
         return fornecedorRepository.save(fornecedorExistente);
     }
